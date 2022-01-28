66be1e43952284461edd53eeaf65e13acb748bef
emmetthough <emmett.hough@gmail.com>
2022-01-28 15:05:48 -0800

    setup fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="gravity_sim", version=1.0,
-        package_dir={"": "lib"}
+        package_dir={"": "lib"},
         author="Emmett Hough",
         author_email="emmett.hough@gmail.com",
         description="Simulation and Analysis for Rotary Attractor Project at Stanford",
